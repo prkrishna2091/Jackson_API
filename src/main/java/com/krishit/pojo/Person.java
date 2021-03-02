f6540a1ce641755132eda87975ca8c760babb279
prkrishna2091 <prkrishna2091@gmail.com>
2021-03-02 22:35:10 +0530

    first commit

---
+++ src/main/java/com/krishit/pojo/Person.java
@@ -0,0 +1,14 @@
+package com.krishit.pojo;
+
+import lombok.Data;
+
+@Data
+public class Person {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String state;
+	private String country;
+	
+
+}
